107a36b6b88218f59ab618e56a7797cef4a9243c
Beau Harrison <beauharrison@gmail.com>
2018-10-18 12:28:44 -0500

    Begin work.

--- summarySelector/summaries.pl
+++ summarySelector/summaries.pl
@@ -1,6 +1,15 @@
-opendir my $dir, "/usr/local/userb/weeklyBeamSummary" or die "Cannot open directory: $!";
+#!/usr/local/bin/perl
+opendir my $dir, "clxsrv:/usr/local/userb/weeklyBeamSummary" or die "Cannot open directory: $!";
+# opendir my $dir, "/Users/beau/Projects/fermi/operations/weeklyBeamSummary" or die "Cannot open directory: $!";
 my @files = readdir $dir;
 closedir $dir;
 
-print "ContentType: text/html;\n\n"
-print @files;
+print "ContentType: text/html\n\n";
+
+print "<html>";
+print "<body>";
+foreach $file (@files) {
+  print '<a href="'.$file.'">'.$file.'</a>';
+}
+print "</html>";
+print "</body>\n";
